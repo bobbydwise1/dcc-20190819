b3f99796423b9ddd02b35555f6fcf2a9aa4bf77e
Robert Lee <bobbydwise1@hotmail.com>
2019-08-19 15:45:32 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Daily Coding Problem 2019-08-19
+
+####  _Good morning! Here's your coding interview problem for today._
+
+This problem was asked by Microsoft.
+
+You have an N by N board. Write a function that, given N, returns the number of possible arrangements of the board where N queens can be placed on the board without threatening each other, i.e. no two queens share the same row, column, or diagonal.
---
+++ scripts.js
@@ -0,0 +1,19 @@
+/*
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Microsoft.
+
+You have an N by N board. Write a function that, given N, returns the number of possible arrangements of the board where N queens can be placed on the board without threatening each other, i.e. no two queens share the same row, column, or diagonal.
+*/
+
+
+
+console.log(root)
+
+$(document).ready(function() {
+  $('#form1').submit(function(event) {
+  event.preventDefault();
+  let input1 = $('#input1').val();
+  $('#output-section-1').text(input1);
+  })
+});
